8c3a93c297e502b0f5633f213c4ca3dfcfc202a8
Jeon Jong Heon <skufmid@gmail.com>
2026-02-24 17:44:28 +0900

    [feat] 캐릭터 방향 전환

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,9 @@ public class Movement : MonoBehaviour
     public float MoveSpeed { get; set; } = 5f;
     public Vector3 MoveDirection { get; set; } = Vector3.zero;
 
-
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position += MoveSpeed * MoveDirection * Time.deltaTime;
+        transform.position += 
+            MoveDirection * MoveSpeed * Time.deltaTime;
     }
 }

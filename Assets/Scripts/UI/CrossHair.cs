8ec4e49e802b5ce28c6c5f572f5896d67a677a99
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2026-02-27 20:50:37 +0900

    [feat] 조준선 추가 (프로토타입용)

---
+++ Assets/Scripts/UI/CrossHair.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+    private float size = 10f;
+    private float thickness = 2f;
+
+    private void OnGUI()
+    {
+        float x = Screen.width / 2f;
+        float y = Screen.height / 2f;
+
+        GUI.DrawTexture(new Rect(x - thickness / 2, y - size, thickness, size * 2), Texture2D.whiteTexture);
+        GUI.DrawTexture(new Rect(x - size, y - thickness / 2, size * 2, thickness), Texture2D.whiteTexture);
+
+    }
+}
